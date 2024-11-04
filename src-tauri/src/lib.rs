c201ffd6572722ac5b3d74451f62ef2a69a51ec0
Swatto <58915441+Swatto86@users.noreply.github.com>
2024-11-04 19:39:29 +0000

    BlackFlickerFix

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use tauri::Runtime;
+use tauri::Manager;
 
 mod commands;
 use crate::commands::*;
@@ -9,6 +10,12 @@ pub fn run<R: Runtime>() where R: Runtime {
         .invoke_handler(tauri::generate_handler![
             calculate_checksum
         ])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            window.set_decorations(true).unwrap();
+            window.show().unwrap();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
